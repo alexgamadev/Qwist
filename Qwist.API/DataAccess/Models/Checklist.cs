1ee8753e7e235d13c0f9c184a4cc22c5e317e742
alex.gama <alex.gama@inseinc.com>
2022-11-16 21:49:43 +0000

    Initial commit

---
+++ Qwist.API/DataAccess/Models/Checklist.cs
@@ -0,0 +1,9 @@
+﻿namespace Qwist.API.DataAccess.Models;
+
+public class Checklist
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = String.Empty;
+    public Guid CreatorId { get; set; }
+    public List<bool> Items { get; set; } = new List<bool>();
+}
---
+++ Qwist.API/DataAccess/Repositories/IChecklistRepository.cs
@@ -0,0 +1,14 @@
+﻿using Qwist.API.DataAccess.Models;
+using System.Collections;
+
+namespace Qwist.API.DataAccess.Repositories;
+
+public interface IChecklistRepository
+{
+    public Task<IEnumerable<Checklist>> GetAllChecklistsAsync(); 
+    public Task<Checklist> GetChecklistAsync(Guid id);
+    public Task CreateChecklistAsync(Checklist checklist);
+    public Task UpdateChecklistAsync(Checklist checklist);
+    public Task RemoveChecklistAsync(Guid id);
+    public Task PatchChecklistAsync(Guid id, Checklist patch);
+}
