0b9a6ff5a4df328e1077ebcd4082e7f8c604e3d0
alex.gama <alex.gama@inseinc.com>
2022-11-16 22:54:24 +0000

    Added comments for checklist repo interface

--- Qwist.API/DataAccess/Repositories/IChecklistRepository.cs
+++ Qwist.API/DataAccess/Repositories/IChecklistRepository.cs
@@ -3,12 +3,50 @@ using System.Collections;
 
 namespace Qwist.API.DataAccess.Repositories;
 
+/// <summary>
+/// The interface for the checklist repository
+/// </summary>
 public interface IChecklistRepository
 {
-    public Task<IEnumerable<Checklist>> GetAllChecklistsAsync(); 
-    public Task<Checklist> GetChecklistAsync(Guid id);
+    /// <summary>
+    /// Gets all checklists 
+    /// </summary>
+    /// <returns>An enumerable of checklists</returns>
+    public Task<IEnumerable<Checklist>> GetAllChecklistsAsync();
+    
+    /// <summary>
+    /// Gets a single checklist with a specified id
+    /// </summary>
+    /// <param name="id">The checklist id</param>
+    /// <returns>A single checklist or null</returns>
+    public Task<Checklist?> GetChecklistAsync(Guid id);
+
+    /// <summary>
+    /// Creates a checklist
+    /// </summary>
+    /// <param name="checklist">The checklist object</param>
+    /// <returns></returns>
     public Task CreateChecklistAsync(Checklist checklist);
+
+    /// <summary>
+    /// Updates an entire checklist
+    /// </summary>
+    /// <param name="checklist">The checklist object</param>
+    /// <returns></returns>
     public Task UpdateChecklistAsync(Checklist checklist);
+
+    /// <summary>
+    /// Removes a checklist with a specified id
+    /// </summary>
+    /// <param name="id">The checklist id</param>
+    /// <returns></returns>
     public Task RemoveChecklistAsync(Guid id);
+
+    /// <summary>
+    /// Partially updates a checklist
+    /// </summary>
+    /// <param name="id">The checklist id</param>
+    /// <param name="patch">The checklist object to patch with</param>
+    /// <returns></returns>
     public Task PatchChecklistAsync(Guid id, Checklist patch);
 }
