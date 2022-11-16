161252ccf6f7f9cebf92f1ec9d24f373405de380
alex.gama <alex.gama@inseinc.com>
2022-11-16 23:28:33 +0000

    Implemented initial checklist controller

---
+++ Qwist.API/Controllers/ChecklistsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using Qwist.API.DataAccess.Models;
+using Qwist.API.DataAccess.Repositories;
+
+namespace Qwist.API.Controllers;
+
+/// <summary>
+/// The checklists controller
+/// </summary>
+[Route("api/checklists")]
+[ApiController]
+public class ChecklistsController : ControllerBase
+{
+    private readonly IChecklistRepository _checklistRepository;
+
+    /// <summary>
+    /// Checklists controller constructor
+    /// </summary>
+    /// <param name="checklistRepository">The injected checklist repository</param>
+    public ChecklistsController(IChecklistRepository checklistRepository)
+    {
+        _checklistRepository = checklistRepository;
+    }
+
+    /// <summary>
+    /// Gets all checklists
+    /// </summary>
+    /// <returns>An action result</returns>
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Checklist>>> GetAllAsync()
+    {
+        var items = await _checklistRepository.GetAllAsync();
+        return Ok(items );
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateAsync(Checklist newChecklist)
+    {
+        await _checklistRepository.CreateAsync(newChecklist);
+        return Ok();
+    }
+}
