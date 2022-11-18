f465cc3cb9c9e870eff73ed49ded0da6803a602a
alex.gama <alex.gama@inseinc.com>
2022-11-18 15:27:13 +0000

    Add placeholder test

---
+++ src/Checklists.Tests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checklists.Tests;
+public class Class1
+{
+    [Fact]
+    public void InitialTest_InputIsTrue_ReturnTrue()
+    {
+        // Arrange
+        bool boolean = true;
+        // Act
+        // Assert
+        Assert.True(boolean);
+    }
+}
